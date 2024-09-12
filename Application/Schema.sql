1225c15857569a7a283dd9fd5c56d339de918f0c
Anujan Kandasamy <84291777+nuj75@users.noreply.github.com>
2024-09-12 01:46:20 +0000

    Pending changes exported from your codespace

---
+++ Application/Schema.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION set_updated_at_to_now() RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ language plpgsql;
+-- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE posts (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL
+);
